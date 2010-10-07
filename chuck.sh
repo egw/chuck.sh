972d98f4116673cee1b3503bd1c7e3fa388be41f
Eric Wong <eric@taedium.com>
2010-10-07 01:59:16 -0700

    split shell script over three lines for readability

--- chuck.sh
+++ chuck.sh
@@ -1,1 +1,3 @@
-curl -Ls 'http://4q.cc/index.php?pid=fact&person=chuck' | perl -ln -0x00 -e 'print $1 if m#"factbox">\s*(.*?)<#s;' | perl -ple 's/\&quot;/"/g;'
+curl -Ls 'http://4q.cc/index.php?pid=fact&person=chuck' | \
+    perl -ln -0x00 -e 'print $1 if m#"factbox">\s*(.*?)<#s;' | \
+    perl -ple 's/\&quot;/"/g;'
